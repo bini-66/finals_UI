9afed7d01d72ce4947dfd3d9307600ed429163b8
Binithi Elvitigala <binithi.vihanga@gmail.com>
2025-01-12 21:38:26 +0530

    test

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace finals_UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //yujyv
         }
     }
 }
