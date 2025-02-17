bb5648d16dae2053885cb2c587351277c8e80005
Shalin <shalinheenkenda@gmail.com>
2025-02-17 19:35:34 +0530

    view feedback and manage employees

---
+++ Model/classes/feedback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finals_UI.Model.classes
+{
+    internal class feedback
+    {
+        public int feedbackId { get; set; }
+        public string feedbackDescription { get; set; }
+        public string rating { get; set; }
+    }
+}
