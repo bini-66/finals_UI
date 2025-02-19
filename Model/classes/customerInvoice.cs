a1ad5e5cec78417a7844ff24aa5073b66e42f068
bini-66 <binithi.vihanga@gmail.com>
2025-02-19 19:31:52 +0530

    converted sales to invoice interface

---
+++ Model/classes/customerInvoice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finals_UI.Model.classes
+{
+    internal class customerInvoice
+    {
+        public int customerInvoiceId {  get; set; }
+        public string invoiceNo {  get; set; }  
+        public DateTime date { get; set; }  
+        public float invoiceTotal {  get; set; }   
+        public int receptionistId {  get; set; }
+        public int vehicleId { get; set; }
+        public int customerId { get; set; }
+
+
+
+
+
+    }
+}
